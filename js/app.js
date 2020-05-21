2f67692fa7ed35cbc4ff927d3857e81e085c8488
unknown <12sanketkumbhar@gmail.com>
2020-05-21 19:27:10 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,93 @@
+const blockElements = document.querySelectorAll('[block-data]')
+const board = document.getElementById('board')
+const winningPrompt = document.getElementById('prompt-box')
+const winningMessage = document.getElementById('prompt-message')
+const restartButton = document.getElementById('restart-button')
+const X_CLASS = 'x'
+const O_CLASS = 'o'
+let oTurn
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+startGame()
+restartButton.addEventListener('click', startGame)
+
+function startGame() {
+    oTurn = false
+    blockElements.forEach(block => {
+        block.classList.remove(X_CLASS)
+        block.classList.remove(O_CLASS)
+        block.removeEventListener('click', handleClick)
+        block.addEventListener('click', handleClick, {
+            once: true
+        })
+    })
+    setBoardHoverClass()
+    winningPrompt.classList.remove('show')
+}
+
+function handleClick(e) {
+    const block = e.target
+    const currentClass = oTurn ? O_CLASS : X_CLASS
+
+    placeMark(block, currentClass)
+
+    if (checkWin(currentClass)) {
+        endGame(false)
+    } else if (isDraw()) {
+        endGame(true)
+    } else {
+        swapTurn()
+        setBoardHoverClass()
+    }
+}
+
+function placeMark(block, currentClass) {
+    block.classList.add(currentClass)
+}
+
+function swapTurn() {
+    oTurn = !oTurn
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS)
+    board.classList.remove(O_CLASS)
+
+    if (oTurn) {
+        board.classList.add(O_CLASS)
+    } else {
+        board.classList.add(X_CLASS)
+    }
+}
+
+function checkWin(currentClass) {
+    return winningCombinations.some(combination => {
+        return combination.every(index => {
+            return blockElements[index].classList.contains(currentClass)
+        })
+    })
+}
+
+function endGame(draw) {
+    if (draw) {
+        winningMessage.innerText = 'Draw !'
+    } else {
+        winningMessage.innerText = `${oTurn ? "O" : "X"} is the winner !`
+    }
+    winningPrompt.classList.add('show')
+}
+
+function isDraw(){
+    return [...blockElements].every(block =>{
+        return block.classList.contains(X_CLASS) || block.classList.contains(O_CLASS)
+    })
+}
